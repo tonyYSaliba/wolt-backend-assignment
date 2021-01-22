0632f08716b4fbc63e4e26daa0dbc0e8160f5f84
TONI SALIBA s275112 <s275112@studenti.polito.it>
2021-01-23 01:17:29 +0200

    Edit db password

--- test/integration/database-utils.ts
+++ test/integration/database-utils.ts
@@ -6,7 +6,7 @@ const testMysqlConfig: Configuration = {
   host: process.env.DB_HOST || '127.0.0.1',
   port: Number(process.env.DB_PORT) || 3306,
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'secret',
+  password: process.env.DB_PASSWORD || '',
   debug: false
 }
 
